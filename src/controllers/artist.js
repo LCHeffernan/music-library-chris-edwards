fd2518e537156f05dd10db4599ec17a1874b068f
Chris Edwards <chrisedwards96@outlook.com>
2023-02-23 11:07:38 +0000

    Finalised code for Update artist tests, Updated getAlllArtists controller to order by ID

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -19,7 +19,7 @@ const createArtist = async (req, res) => {
 
 const getAllArtists = async (_, res) => {
   try {
-    const { rows } = await db.query('SELECT * FROM Artists');
+    const { rows } = await db.query('SELECT * FROM Artists ORDER by id ASC');
     res.status(200).json(rows);
   } catch (err) {
     res.status(500).json(err.message);
@@ -45,8 +45,40 @@ const getArtistById = async (req, res) => {
   }
 };
 
+const updateArtist = async (req, res) => {
+  const { id } = req.params;
+  const { name, genre } = req.body;
+
+  let query, params;
+
+  if (name && genre) {
+    query = `UPDATE Artists SET name = $1, genre = $2 WHERE id = $3 RETURNING *`;
+    params = [name, genre, id];
+  } else if (name) {
+    query = `UPDATE Artists SET name = $1 WHERE id = $2 RETURNING *`;
+    params = [name, id];
+  } else if (genre) {
+    query = `UPDATE Artists SET genre = $1 WHERE id = $2 RETURNING *`;
+    params = [genre, id];
+  }
+  try {
+    const {
+      rows: [artist],
+    } = await db.query(query, params);
+
+    if (!artist) {
+      return res.status(404).json({ message: `Artist ${id} does not exist` });
+    }
+    res.status(200).json(artist);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err.message);
+  }
+};
+
 module.exports = {
   createArtist,
   getAllArtists,
   getArtistById,
+  updateArtist,
 };
