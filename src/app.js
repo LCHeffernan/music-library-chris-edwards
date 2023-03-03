243f1a1ee7e5ff0092999877cd7ae2b89a2302c4
Chris Edwards <chrisedwards96@outlook.com>
2023-03-03 16:09:14 +0000

    added swagger for gui

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const artistRouter = require('./routes/artist');
 const albumsRouter = require('./routes/album');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 const cors = require('cors');
 const app = express();
 const morgan = require('morgan');
+const path = require('path');
 const helmet = require('helmet');
 
 app.use(express.json());
@@ -11,6 +14,19 @@ app.use(cors());
 app.use(morgan('tiny'));
 app.use(helmet());
 
+const options = {
+  swaggerDefinition: {
+    info: {
+      title: 'Music Library',
+      version: '1.0.0',
+      description: 'A simple music library database API',
+    },
+  },
+  apis: [path.join(__dirname, '/routes/*.js')],
+};
+const swaggerSpecs = swaggerJsdoc(options);
+
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 app.use('/artists', artistRouter);
 app.use('/albums', albumsRouter);
 
